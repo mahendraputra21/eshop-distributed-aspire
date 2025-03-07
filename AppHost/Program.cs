1aef05e08cece761ef76e78cba68f396cce10460
AzureAD\DewaGdeMahendraPutra <d.putra@lakeba.com>
2025-03-07 11:36:04 +0800

    Add catalog project

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,5 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // add projects and cloud-native backing services here
+builder.AddProject<Projects.catalog>("catalog");
 
 builder.Build().Run();
