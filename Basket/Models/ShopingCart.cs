5a88052fe40c14189ba9764a3e045e721a16f35e
AzureAD\DewaGdeMahendraPutra <d.putra@lakeba.com>
2025-03-14 15:34:08 +0800

    Develop SC Domain Entity Models in Basket Microservice Domain Models

---
+++ Basket/Models/ShopingCart.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.Models;
+
+public class ShopingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShopingCartItem> Items { get; set; } = new();
+
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
