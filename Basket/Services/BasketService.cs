948a640791d4781c1ca434a0b4b3baefa524154d
AzureAD\DewaGdeMahendraPutra <d.putra@lakeba.com>
2025-03-15 10:22:12 +0800

    Integrate w/ Catalog into BasketService.cs using CatalogApiClient

--- Basket/Services/BasketService.cs
+++ Basket/Services/BasketService.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 
 namespace Basket.Services;
 
-public class BasketService(IDistributedCache cache)
+public class BasketService(IDistributedCache cache, CatalogApiClient catalogApiClient)
 {
     public async Task<ShoppingCart?> GetBasket(string userName)
     {
@@ -14,6 +14,15 @@ public class BasketService(IDistributedCache cache)
 
     public async Task UpdateBasket(ShoppingCart basket)
     {
+        // Before update(Add/remove Item) into SC, we should call Catalog ms GetProductById method
+        // Get latest product information and set Price and ProductName when adding item into SC
+        foreach (var item in basket.Items) 
+        {
+            var product = await catalogApiClient.GetProductById(item.ProductId);
+            item.Price = product.Price;
+            item.ProductName = product.Name;
+        }
+
         await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket));
     }
 
