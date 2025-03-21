7322ec37666e2cee91fc4efa302c79ace51496af
AzureAD\DewaGdeMahendraPutra <d.putra@lakeba.com>
2025-03-21 14:54:16 +0800

    Create Shared Messaging Folders and Classes for ProductPriceChangedIntegration

---
+++ ServiceDefaults/Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace ServiceDefaults.Messaging.Events;
+public record IntegrationEvent
+{
+    public Guid EventId => Guid.NewGuid();
+    public DateTime OccuredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
