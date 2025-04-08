c1513fe9544a783a48046b9020089b10321b828b
AzureAD\DewaGdeMahendraPutra <d.putra@lakeba.com>
2025-04-08 10:31:26 +0800

    Develop Traditional Search in ProductService.cs Business Class

--- catalog/Services/ProductAIService.cs
+++ catalog/Services/ProductAIService.cs
@@ -4,7 +4,7 @@ namespace Catalog.Services;
 
 public class ProductAIService(IChatClient chatClient)
 {
-    public Task<string> SupportAsync(string query)
+    public async Task<string> SupportAsync(string query)
     {
         var systemPrompt = """
         You are a useful assistant. 
@@ -22,7 +22,15 @@ public class ProductAIService(IChatClient chatClient)
             new(ChatRole.User, query)
         };
 
-        //var resultPrompt = await chatClient.CompleteAsync(chatHistory);
-        return Task.FromResult("");
+        var resultPrompt = await chatClient.GetResponseAsync(chatHistory);
+        return resultPrompt.Message.Contents[0].ToString()!;
+    }
+
+    public async Task<IEnumerable<Product>> SearchProductAsync(string query)
+    {
+        // use EmbeddingGenerator
+        // use InMemoryVectorStore
+        // provide semantic search
+
     }
 }
