35a8ba1503862023e45c6cb3a23dd0986e925a78
AzureAD\DewaGdeMahendraPutra <d.putra@lakeba.com>
2025-04-08 10:37:04 +0800

    Register EmbeddingGenerator and VectorStore services in Catalog/Program.cs

--- catalog/Program.cs
+++ catalog/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.SemanticKernel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +10,12 @@ builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<ProductAIService>();
 builder.Services.AddMassTransitWithAssemblies(Assembly.GetExecutingAssembly());
 
+// Register Ollama-based chat & embedding
 builder.AddOllamaSharpChatClient("ollama-llama3-2");
+builder.AddOllamaSharpEmbeddingGenerator("ollama-all-minilm");
+
+// Register an in-memory vector store
+builder.Services.AddInMemoryVectorStoreRecordCollection<int, ProductVector>("products");
 
 var app = builder.Build();
 
